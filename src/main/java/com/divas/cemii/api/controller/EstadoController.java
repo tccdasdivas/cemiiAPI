355d7d51d19a740d2a4a4329c20a86d2f6abe4f9
eduardapac <eduardaleitepac@gmail.com>
2025-05-14 12:21:31 -0300

    14/05

--- src/main/java/com/divas/cemii/api/controller/EstadoController.java
+++ src/main/java/com/divas/cemii/api/controller/EstadoController.java
@@ -1,4 +1,22 @@
 package com.divas.cemii.api.controller;
 
+
+import com.divas.cemii.domain.model.Estado;
+import com.divas.cemii.domain.repository.EstadoRepository;
+import com.divas.cemii.domain.service.EstadoService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/estados")
+@RestController
 public class EstadoController {
+    private EstadoRepository estadoRepository;
+
+    private EstadoService estadoService;
+
+    public List<Estado> listar(){
+        return estadoRepository.findAll();
+    }
 }
